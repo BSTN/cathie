a00608f0eb08190a8d9c2b4693e345976a676b63
BSTN <basten@bstn.nl>
2025-01-12 22:29:33 +0000

    update

--- modules/editor/components/fields/text.vue
+++ modules/editor/components/fields/text.vue
@@ -4,6 +4,7 @@
       type="text"
       class="w-[32rem] max-w-full rounded bg-bg1 px-4 py-2 placeholder-fg2 focus:bg-bg2 focus:outline-none"
       v-model="model"
+      @keydown.escape.stop="$event.target.blur()"
     />
   </div>
 </template>
