a55ed560db5ef3d5dfa9ffc3445218cbd2fccaae
BSTN <basten@bstn.nl>
2025-01-12 22:29:20 +0000

    update

--- modules/editor/components/fields/markdown.vue
+++ modules/editor/components/fields/markdown.vue
@@ -3,6 +3,7 @@
     <textarea
       class="min-h-[20rem] w-[40rem] max-w-full rounded-md bg-bg1 p-4 focus:bg-bg2 focus:outline-none"
       v-model="model"
+      @keydown.escape.stop="$event.target.blur()"
     ></textarea>
   </div>
 </template>
